89bca778f2bd676189dcbb98fd3fe8d8466a9ed6
Taka Oyama <toyama4649@hotmail.com>
2022-08-12 22:53:09 +0900

    initial commit

---
+++ tests/src/TestCase.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Kirameki\Cli;
+
+use Kirameki\Container\Container;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+use function array_sum;
+
+class TestCase extends BaseTestCase
+{
+}
