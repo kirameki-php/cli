576c41d76284a7ebd2f9ea6d3239a0eafac96547
Taka Oyama <taka.oyama@gmail.com>
2023-09-29 13:34:00 +0900

    add input test

---
+++ tests/src/InputTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Kirameki\Cli;
+
+use Kirameki\Cli\Input;
+use Kirameki\Stream\TmpFileStream;
+use SouthPointe\Ansi\Stream;
+use function dump;
+use const PHP_EOL;
+
+final class InputTest extends TestCase
+{
+    public function test_integer(): void
+    {
+        $outStream = new TmpFileStream();
+        $inStream = new TmpFileStream();
+
+        $input = new Input(
+            $inStream,
+            new Stream($outStream->getResource()),
+        );
+
+        $inStream->write('123' . PHP_EOL);
+        $inStream->rewind();
+
+        $this->assertSame(123, $input->integer('in:'));
+
+        dump($outStream->rewind()->readToEnd());
+    }
+
+    public function test_hidden(): void
+    {
+        $outStream = new TmpFileStream();
+        $inStream = new TmpFileStream();
+
+        $input = new Input(
+            $inStream,
+            new Stream($outStream->getResource()),
+        );
+
+        $inStream->write('123' . PHP_EOL);
+        $inStream->write('123' . PHP_EOL);
+        $inStream->write('123' . PHP_EOL);
+        $inStream->rewind();
+
+        $this->assertSame('123', $input->hidden('hidden:'));
+    }
+}
