a1cbaafcafb860fdb4360cf98521910254577bf5
Taka Oyama <taka.oyama@gmail.com>
2023-10-06 19:36:15 +0900

    more test

--- tests/src/Input/LineReaderTest.php
+++ tests/src/Input/LineReaderTest.php
@@ -33,4 +33,87 @@ final class LineReaderTest extends TestCase
 
         $this->assertSame('', $reader->readline());
     }
+
+    public function test_escape_delete(): void
+    {
+        $outStream = new TmpFileStream();
+        $inStream = new TmpFileStream();
+        $reader = new LineReader($inStream, new Stream($outStream->getResource()));
+
+        // 1 + cursor back + delete
+        $inStream->write("1\x02\x04" . PHP_EOL);
+        $inStream->rewind();
+
+        $this->assertSame('', $reader->readline());
+    }
+
+    public function test_escape_cut_to_bol(): void
+    {
+        $outStream = new TmpFileStream();
+        $inStream = new TmpFileStream();
+        $reader = new LineReader($inStream, new Stream($outStream->getResource()));
+
+        // 1 + cursor back + cut to beginning of line
+        $inStream->write("1\x15" . PHP_EOL);
+        $inStream->rewind();
+
+        $this->assertSame('', $reader->readline());
+        $this->assertSame('1', $reader->clipboard);
+    }
+
+    public function test_escape_cut_to_eol(): void
+    {
+        $outStream = new TmpFileStream();
+        $inStream = new TmpFileStream();
+        $reader = new LineReader($inStream, new Stream($outStream->getResource()));
+
+        // 1 + cursor back + cut to end of line
+        $inStream->write("1\x02\x0b" . PHP_EOL);
+        $inStream->rewind();
+
+        $this->assertSame('', $reader->readline());
+        $this->assertSame('1', $reader->clipboard);
+    }
+
+    public function test_escape_cut_word(): void
+    {
+        $outStream = new TmpFileStream();
+        $inStream = new TmpFileStream();
+        $reader = new LineReader($inStream, new Stream($outStream->getResource()));
+
+        // hello world + cut word
+        $inStream->write("hello world\x17" . PHP_EOL);
+        $inStream->rewind();
+
+        $this->assertSame('hello ', $reader->readline());
+        $this->assertSame('world', $reader->clipboard);
+    }
+
+    public function test_escape_cut_word_at_beginning(): void
+    {
+        $outStream = new TmpFileStream();
+        $inStream = new TmpFileStream();
+        $reader = new LineReader($inStream, new Stream($outStream->getResource()));
+
+        // hello world + cut word
+        $inStream->write("hello world\x01\x17" . PHP_EOL);
+        $inStream->rewind();
+
+        $this->assertSame('hello world', $reader->readline());
+        $this->assertSame('', $reader->clipboard);
+    }
+
+    public function test_escape_cut_word_with_trailing_spaces(): void
+    {
+        $outStream = new TmpFileStream();
+        $inStream = new TmpFileStream();
+        $reader = new LineReader($inStream, new Stream($outStream->getResource()));
+
+        // hello world + cut word
+        $inStream->write("hello world   \x17" . PHP_EOL);
+        $inStream->rewind();
+
+        $this->assertSame('hello ', $reader->readline());
+        $this->assertSame('world   ', $reader->clipboard);
+    }
 }
