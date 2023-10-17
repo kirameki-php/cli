96004830890a6281c432ab09173f100e83f427f6
Taka Oyama <taka.oyama@gmail.com>
2023-10-17 23:52:07 +0900

    cache alias

--- test.php
+++ test.php
@@ -1,9 +1,6 @@
 <?php
 
-$string = 'Some of "this string is\"" in quotes';
-$arr = preg_split('/("[^"]*")|\h+/', $string, -1, PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE);
-
-var_dump($arr);
+var_dump(array_diff(['a' => 'a', 'b' => 'b'], ['a', 'c']));
 
 //
 //use Kirameki\Cli\Input;
